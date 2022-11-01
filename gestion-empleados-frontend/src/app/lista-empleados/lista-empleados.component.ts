ceb4bba48da474d1d3556e295327fd633f40bd77
Andrea Aranda Gaviria <andrea.aranda@ganesuperservicios.co>
2022-11-01 18:02:40 -0500

    crudMicroservicios

--- gestion-empleados-frontend/src/app/lista-empleados/lista-empleados.component.ts
+++ gestion-empleados-frontend/src/app/lista-empleados/lista-empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../empleado';
 
 @Component({
   selector: 'app-lista-empleados',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaEmpleadosComponent implements OnInit {
 
+  empleados:Empleado[];
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.empleados=[{
+      "id":2,
+      "nombre" : "Christian",
+      "apellido" : "Marinez",
+      "email" : "cm@gmail.com"
+    },
+    {
+      "id":3,
+      "nombre" : "Julian",
+      "apellido" : "Ramirez",
+      "email" : "jr@gmail.com"
+    }
+    ];
   }
 
 }
