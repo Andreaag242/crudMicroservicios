354cb84717dbc7e4c82c96a9adee6155cdab8f00
Andreaag242 <aaranda96@misena.edu.co>
2022-11-02 22:41:33 -0500

    crudEmpleado

--- gestion-empleados-frontend/src/app/lista-empleados/lista-empleados.component.ts
+++ gestion-empleados-frontend/src/app/lista-empleados/lista-empleados.component.ts
@@ -1,3 +1,4 @@
+import swal from 'sweetalert2';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Empleado } from '../empleado';
@@ -23,10 +24,31 @@ export class ListaEmpleadosComponent implements OnInit {
   }
   
   eliminarEmpleado(id:number){
-    this.empleadoService.eliminarEmpleado(id).subscribe(dato =>{
-      console.log(dato);
-      this.obtenerEmpleados();
-    });
+    swal({
+      title: '¿Estas seguro?',
+      text: "Confirma si deseas eliminar al empleado",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si , elimínalo',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: true
+    }).then((result) => {
+      if(result.value){
+        this.empleadoService.eliminarEmpleado(id).subscribe(dato => {
+          console.log(dato);
+          this.obtenerEmpleados();
+          swal(
+            'Empleado eliminado',
+            'El empleado ha sido eliminado con exito',
+            'success'
+          )
+        })
+      }
+    })
   }
   private obtenerEmpleados(){
     this.empleadoService.obtenerListaDeEmpleados().subscribe(dato =>{
